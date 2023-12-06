8072118b8f1e839c5ea9fb1ed047e23daf76b279
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-06 18:39:29 +0500

    GetEmployeeCommands added

---
+++ src/TurarJoy/TurarJoy.Application/UseCases/Employees/Queries/GetEmployeeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TurarJoy.Domain.Entities;
+
+namespace TurarJoy.Application.UseCases.Employees.Queries
+{
+    public class GetEmployeeCommand : IRequest<IList<Employee>>
+    {
+
+    }
+}
