1b02f2c04584dcb19ddd8a2d0e69cd1f39809dfe
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-06 21:53:08 +0500

    Handlers added

---
+++ src/TurarJoy/TurarJoy.Application/UseCases/HouseBuildingCompanys/Handlers/GetHouseCompanyHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TurarJoy.Application.Abstractions;
+using TurarJoy.Application.UseCases.HouseBuildingCompanys.Queries;
+using TurarJoy.Domain.Entities;
+
+namespace TurarJoy.Application.UseCases.HouseBuildingCompanys.Handlers
+{
+    public class GetHouseCompanyHandler : IRequestHandler<GetHouseCompanyQuery, IList<HouseBuildingCompany>>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public GetHouseCompanyHandler(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IList<HouseBuildingCompany>> Handle(GetHouseCompanyQuery request, CancellationToken cancellationToken)
+        {
+            var company = await _applicationDbContext.HouseBuildingCompany.ToListAsync(cancellationToken);
+            return company;
+        }
+    }
+}
