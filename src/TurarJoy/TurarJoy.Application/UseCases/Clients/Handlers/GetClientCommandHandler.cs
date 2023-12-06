655a00b1b9f8f1387de877a40ab3633d2979b5ca
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-06 10:43:26 +0500

    GetClientCommandHandler added

---
+++ src/TurarJoy/TurarJoy.Application/UseCases/Clients/Handlers/GetClientCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TurarJoy.Application.Abstractions;
+using TurarJoy.Application.UseCases.Clients.Queries;
+using TurarJoy.Domain.Entities;
+
+namespace TurarJoy.Application.UseCases.Clients.Handlers
+{
+    public class GetClientCommandHandler : IRequestHandler<GetClientCommand, IList<Client>>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public GetClientCommandHandler(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IList<Client>> Handle(GetClientCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _applicationDbContext.Clients.ToListAsync();
+            return result;
+        }
+    }
+}
