4660e8427c44411b04de22648f3abb80cc269210
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 01:58:13 +0500

    Configurations upgrated

--- src/TurarJoy/TurarJoy.Infrastructure/Persistance/EntitiesConfiguration/SaleConfiguration.cs
+++ src/TurarJoy/TurarJoy.Infrastructure/Persistance/EntitiesConfiguration/SaleConfiguration.cs
@@ -3,12 +3,18 @@ using TurarJoy.Domain.Entities;
 
 namespace TurarJoy.Infrastructure.Persistance.EntitiesConfiguration
 {
-    public class HouseConfiguration
+    public class SaleConfiguration
     {
         public void Configure(EntityTypeBuilder<Sale> builder)
         {
             builder.HasIndex(x => x.HouseId)
                 .IsUnique();
+            builder.HasOne(x => x.House)
+                .WithMany(z => z.Sales);
+            builder.HasOne(x => x.Client)
+                .WithMany(z => z.Sales);
+            builder.HasOne(x => x.Employee)
+                .WithMany(z => z.Sales);
         }
     }
 }
