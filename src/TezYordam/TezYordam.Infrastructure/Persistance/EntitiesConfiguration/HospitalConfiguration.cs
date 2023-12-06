b668ed3a5aa2c2a72311640cc91fe61573170f45
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 04:45:19 +0500

    EntityConfigurations added

---
+++ src/TezYordam/TezYordam.Infrastructure/Persistance/EntitiesConfiguration/HospitalConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Infrastructure.Persistance.EntitiesConfiguration
+{
+    public class HospitalConfiguration : IEntityTypeConfiguration<Hospital>
+    {
+        public void Configure(EntityTypeBuilder<Hospital> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasData(new Hospital
+            {
+                Id = 1,
+                Name = "SebzorTTB",
+                Address = "Sebzor tumani",
+                Phone = "+998713254565",
+                Email = "sebzorttb@gmail.com"
+            });
+        }
+    }
+}
---
+++ src/TezYordam/TezYordam.Infrastructure/Persistance/EntitiesConfiguration/PatientConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Infrastructure.Persistance.EntitiesConfiguration
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasMany(x => x.Calls)
+                .WithOne(x => x.Patient);
+        }
+    }
+}
