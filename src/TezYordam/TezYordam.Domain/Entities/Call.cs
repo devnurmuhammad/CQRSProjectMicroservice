da31508ae0b3371efca3c79eb8cbd32b11bcc891
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 04:44:49 +0500

    TezYordam entites added

---
+++ src/TezYordam/TezYordam.Domain/Entities/Call.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TezYordam.Domain.Entities
+{
+    public class Call
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = default!;
+        public DateTime CalledAt { get; set; }
+        public DateTime ArrivedAt { get; set; }
+        [ForeignKey("PatientId")]
+        public int PatientId { get; set; }
+        public Patient? Patient { get; set; }
+        [ForeignKey("CarId")]
+        public int CarId { get; set; }
+        public Car? Car { get; set; }
+        [ForeignKey("DoctorId")]
+        public int DoctorId { get; set; }
+        public Doctor? Doctor { get; set; }
+    }
+}
