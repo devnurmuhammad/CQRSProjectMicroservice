3fb73113634a0ede289a4a0cd5a95f5880057e26
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 04:45:38 +0500

    persistance added

---
+++ src/TezYordam/TezYordam.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TezYordam.Application.Abstractions;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Infrastructure.Persistance;
+
+public class ApplicationDbContext : DbContext, ITezYordamApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Hospital> Hospitals { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Call> Calls { get; set; }
+}
