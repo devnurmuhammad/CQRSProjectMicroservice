a6ee132269b79c390c62f67afd7fd400cf29b27f
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-06 10:46:50 +0500

    ClientDTO added

---
+++ src/TurarJoy/TurarJoy.Web/ViewModels/ClientDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TurarJoy.Web.ViewModels
+{
+    public class ClientDTO
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string PassportNumber { get; set; } = default!;
+        public int Age { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
