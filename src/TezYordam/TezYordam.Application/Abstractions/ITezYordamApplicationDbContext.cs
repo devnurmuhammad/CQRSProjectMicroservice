bc1a6dfddf7967ebc0dd26db3b3844f94706ddb8
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 04:44:01 +0500

    abstractions added

---
+++ src/TezYordam/TezYordam.Application/Abstractions/ITezYordamApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Application.Abstractions
+{
+    public interface ITezYordamApplicationDbContext
+    {
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Call> Calls { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
