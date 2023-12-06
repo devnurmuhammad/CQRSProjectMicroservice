703a6b955b0ef4692a95577a66ea654ae914bc00
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 01:56:16 +0500

    ClientHamdlers upgrated

--- src/TurarJoy/TurarJoy.Application/UseCases/Clients/Handlers/GetClientCommandByIdHandler.cs
+++ src/TurarJoy/TurarJoy.Application/UseCases/Clients/Handlers/GetClientCommandByIdHandler.cs
@@ -17,7 +17,10 @@ namespace TurarJoy.Application.UseCases.Clients.Handlers
 
         public async Task<Client> Handle(GetClientCommandById request, CancellationToken cancellationToken)
         {
-            var client = await _applicationDbContext.Clients.FirstOrDefaultAsync(x => x.Id == request.Id);
+            var client = await _applicationDbContext.Clients
+                .Include(x => x.Sales)
+                .ThenInclude(y => y.Client)
+                .FirstOrDefaultAsync(x => x.Id == request.Id);
             if (client == null)
             {
                 return new Client();
