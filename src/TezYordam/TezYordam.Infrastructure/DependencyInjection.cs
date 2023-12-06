bb5dc65a82fd94bc435b1f5b91b80707078a114c
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 04:45:56 +0500

    infrastructure dependency added

---
+++ src/TezYordam/TezYordam.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TezYordam.Application.Abstractions;
+using TezYordam.Infrastructure.Persistance;
+
+namespace TezYordam.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ITezYordamApplicationDbContext, ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+        });
+
+        return services;
+    }
+}
