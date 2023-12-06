87264e8f2373a6423088d570444fd3b032adcfb0
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 01:58:35 +0500

    SaleDTO added

---
+++ src/TurarJoy/TurarJoy.Web/ViewModels/SaleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TurarJoy.Web.ViewModels
+{
+    public class SaleDTO
+    {
+        public DateTime SaledAt { get; set; }
+        public string? Cost { get; set; }
+        public int EmployeeId { get; set; }
+        public int ClientId { get; set; }
+        public int HouseId { get; set; }
+    }
+}
