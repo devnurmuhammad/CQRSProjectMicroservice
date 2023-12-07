32d6c5cf888c3be6dc1738f4f1759ccfd797ceaa
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 10:56:24 +0500

    call controller and hospital controllers added

---
+++ src/TezYordam/TezYordam.Application/UseCases/Hospitals/Queries/GetHospitalQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Application.UseCases.Hospitals.Queries
+{
+    public class GetHospitalQuery : IRequest<IList<Hospital>>
+    {
+    }
+}
