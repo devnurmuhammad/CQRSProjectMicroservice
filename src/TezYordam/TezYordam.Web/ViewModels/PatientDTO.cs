bc02e3692c0de0911332efdebd2b498eb67a2d70
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 09:30:54 +0500

    view model added

---
+++ src/TezYordam/TezYordam.Web/ViewModels/PatientDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TezYordam.Web.ViewModels
+{
+    public class PatientDTO
+    {
+        public string FirstName { get; set; } = default!;
+        public string? LastName { get; set; }
+        public string Phone { get; set; } = default!;
+        public string Address { get; set; } = default!;
+    }
+}
