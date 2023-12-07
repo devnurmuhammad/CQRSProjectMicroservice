5f739a7d45e364190310b3f3374cf1e4db307619
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 08:16:00 +0500

    CarHandlers added

---
+++ src/TezYordam/TezYordam.Application/UseCases/Cars/Handlers/DeleteCarCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TezYordam.Application.Abstractions;
+using TezYordam.Application.UseCases.Cars.Commands;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Application.UseCases.Cars.Handlers
+{
+    public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand, bool>
+    {
+        private readonly ITezYordamApplicationDbContext _context;
+
+        public DeleteCarCommandHandler(ITezYordamApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+        {
+            Car? car = await _context.Cars.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (car == null)
+            {
+                return false;
+            }
+            _context.Cars.Remove(car);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return true;
+        }
+    }
+}
---
+++ src/TezYordam/TezYordam.Application/UseCases/Cars/Handlers/GetCarByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TezYordam.Application.Abstractions;
+using TezYordam.Application.UseCases.Cars.Queries;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Application.UseCases.Cars.Handlers
+{
+    public class GetCarByIdQueryHandler : IRequestHandler<GetCarByIdQuery, Car>
+    {
+        private readonly ITezYordamApplicationDbContext _context;
+
+        public GetCarByIdQueryHandler(ITezYordamApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Car> Handle(GetCarByIdQuery request, CancellationToken cancellationToken)
+        {
+            Car? car = await _context.Cars
+                .Include(c => c.Calls)
+                .ThenInclude(x => x.Doctor)
+                .FirstOrDefaultAsync(z => z.Id == request.Id);
+
+            if (car == null)
+            {
+                return new Car();
+            }
+            return car;
+        }
+    }
+}
