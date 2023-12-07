ad2550bef784070177b3aca685d9990429fe998e
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 10:55:07 +0500

    call handlers added

---
+++ src/TezYordam/TezYordam.Application/UseCases/Calls/Handlers/GetCallHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TezYordam.Application.Abstractions;
+using TezYordam.Application.UseCases.Calls.Queries;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Application.UseCases.Calls.Handlers
+{
+    public class GetCallHandler : IRequestHandler<GetCallQuery, IList<Call>>
+    {
+        private readonly ITezYordamApplicationDbContext _context;
+
+        public GetCallHandler(ITezYordamApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<Call>> Handle(GetCallQuery request, CancellationToken cancellationToken)
+        {
+            IList<Call> calls = await _context.Calls
+                .Include(x => x.Car)
+                .ThenInclude(z => z.Calls)
+                .ThenInclude(c => c.Doctor)
+                .ThenInclude(v => v.Calls)
+                .ThenInclude(b => b.Patient)
+                .ToListAsync(cancellationToken);
+            return calls;
+        }
+    }
+}
