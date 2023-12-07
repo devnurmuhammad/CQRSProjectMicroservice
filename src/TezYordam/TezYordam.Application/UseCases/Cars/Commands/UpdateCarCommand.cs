b273a061a6c5068cf209e01b94df4b0bfeb60f19
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 08:15:49 +0500

    CarCommands added

---
+++ src/TezYordam/TezYordam.Application/UseCases/Cars/Commands/UpdateCarCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TezYordam.Application.UseCases.Cars.Commands
+{
+    public class UpdateCarCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Number { get; set; } = default!;
+        public string Model { get; set; } = default!;
+    }
+}
