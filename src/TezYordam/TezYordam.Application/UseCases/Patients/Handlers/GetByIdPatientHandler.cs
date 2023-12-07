99e3a318c649e004d798dc3aea8e7e8b4bd96f97
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 09:29:54 +0500

    PatientHandlers added

---
+++ src/TezYordam/TezYordam.Application/UseCases/Patients/Handlers/GetByIdPatientHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TezYordam.Application.Abstractions;
+using TezYordam.Application.UseCases.Patients.Queries;
+using TezYordam.Domain.Entities;
+
+namespace TezYordam.Application.UseCases.Patients.Handlers
+{
+    public class GetByIdPatientHandler : IRequestHandler<GetPatientByIdQuery, Patient>
+    {
+        private readonly ITezYordamApplicationDbContext _context;
+
+        public GetByIdPatientHandler(ITezYordamApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Patient> Handle(GetPatientByIdQuery request, CancellationToken cancellationToken)
+        {
+            Patient? patient = await _context.Patients.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (patient == null)
+            {
+                return new Patient();
+            }
+            return patient;
+        }
+    }
+}
---
+++ src/TezYordam/TezYordam.Application/UseCases/Patients/Handlers/UpdatePatientHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TezYordam.Application.Abstractions;
+using TezYordam.Application.UseCases.Patients.Commands;
+
+namespace TezYordam.Application.UseCases.Patients.Handlers
+{
+    public class UpdatePatientHandler : IRequestHandler<UpdatePatientCommand, bool>
+    {
+        private readonly ITezYordamApplicationDbContext _context;
+
+        public UpdatePatientHandler(ITezYordamApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(UpdatePatientCommand request, CancellationToken cancellationToken)
+        {
+            var patient = await _context.Patients.FirstOrDefaultAsync(z => z.Id == request.Id);
+            if (patient == null)
+            {
+                return false;
+            }
+            patient.FirstName = request.FirstName;
+            patient.LastName = request.LastName;
+            patient.Address = request.Address;
+            patient.Phone = request.Phone;
+
+            _context.Patients.Update(patient);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
