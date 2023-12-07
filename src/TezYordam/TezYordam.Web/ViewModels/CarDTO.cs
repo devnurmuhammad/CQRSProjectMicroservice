6230db85044a0061012c11dc5583a100f0b1d985
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-07 08:19:45 +0500

    car and doctorviewmodels added

---
+++ src/TezYordam/TezYordam.Web/ViewModels/CarDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TezYordam.Web.ViewModels
+{
+    public class CarDTO
+    {
+        public string Name { get; set; } = default!;
+        public string Number { get; set; } = default!;
+        public string Model { get; set; } = default!;
+    }
+}
