578ed6e50a5dd0f612f788a25ade2de028b4c69b
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-12-08 20:39:25 +0500

    cashing added, but bot useable

--- src/TurarJoy/TurarJoy.Application/CashingService/TokenService.cs
+++ src/TurarJoy/TurarJoy.Application/CashingService/TokenService.cs
@@ -4,7 +4,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace JWTGetToken.Services
+namespace TurarJoy.Application.CashingService
 {
     public class TokenService : ITokenService
     {
